535e251cf647fa51e9eec6ea222eeafb20ddec1a
Marco Ammon <ammon.marco@t-online.de>
2017-05-14 14:46:57 +0200

    Fixed rendering, bumped version

--- SpecialShowMatch.php
+++ SpecialShowMatch.php
@@ -37,24 +37,49 @@ class SpecialShowMatch extends SpecialPage
 
 		    $output->addWikiText( "An error occured!: " . $_result['error'] );
 	    } else {
-		    $output->addHTML($this->sandboxParse( $this->generateWikicode($_result)) );
+	    	$wikitext = $this->generateWikicode($_result, $matchId);
+		    $output->addHTML($this->sandboxParse( $wikitext) );
 	    }
 
     }
 
-    private function generateWikicode($result){
-	    $wikicode = "<table class=\"wikitable collapsible collapsed matchseries-table-2\">
-						<tr>
-							<th colspan=4 class=\"date\">" . $result->start_time . "</th>
-						</tr>
-						{{Match series game |radiant=left"
-		    ."|team1=" . $result->teams['radiant'] . "|team1Kills=" . $result->kills['radiant']
-		    ."|team2=" . $result->teams['dire'] . "|team2Kills=" . $result->kills['dire']
-		    ."|winner=" . (($result->radiant_win == true)? 1 : 2 ). "}}"
-	        ."</table>";
-
-
-	    return $wikicode;
+    private function generateWikicode($result, $matchId)
+    {
+	    $sides = array("radiant", "dire");
+	    $player_stats = array("name", "hero", "level", "kills", "deaths", "assists", "last_hits",
+		    "denies", "gold_per_min", "xp_per_min");
+	    $items = array("item", "bearitem");
+	    $template = "{{Detailed game stats";
+	    $pick_bans = array("pick", "ban");
+	    $template .= "|date=" . $result->start_time . " ";
+	    $template .= "|winner=" . (($result->radiant_win == true) ? 1 : 2) . " ";
+	    $template .= "|matchid=" . $matchId . " ";
+	    foreach ($sides as $side) {
+	    	$template .= "|" . $side . "_team=" . $result->teams[$side] . " ";
+		    $picks_bans_values = $result->picks_bans[$side];
+	    	foreach ($pick_bans as $stat) {
+			    for ($i = 1; $i <= 5; $i++) {
+				    $template .= "|" . $side . "_" . $stat . "_" . $i . "=" . $picks_bans_values[$stat . "_" . $i] . " ";
+			    }
+		    }
+		    $kills = $result->kills[$side];
+	    	$template .= "|" . $side . "_" . "kills=" . $kills . " ";
+	    	$deaths = $result->deaths[$side];
+	    	$template .= "|" . $side . "_" . "deaths=" . $deaths . " ";
+	    	$player_values = $result->players[$side];
+		    for ($i = 1; $i <= 5; $i++) {
+		    	foreach ($player_stats as $stat) {
+					$template .= "|" . $side . "_" . $stat . "_" . $i . "=" . $player_values["player_" . $i]->$stat . " ";
+				}
+				foreach ($items as $item){
+		    		for ($j = 1; $j <= 6; $j++) {
+					    $template .= "|" . $side . "_" . $i . "_" . $item . "_" . $j . "=" . $player_values["player_" . $i]->{$item . "_" . $j} . " ";
+				    }
+				}
+			}
+	    }
+		$template .= "}}";
+		return $template;
     }
 
     private function sandboxParse($wikiText) {
