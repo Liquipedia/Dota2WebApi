65b72eae7f2882724668df4ddfa2e0d3aa7c00ad
FO-nTTaX <fo.nttax@yahoo.de>
2017-12-15 13:39:06 +0100

    fml

--- Dota2WebApi.alias.php
+++ Dota2WebApi.alias.php
@@ -1,19 +1,16 @@
 <?php
-
-class Dota2WebApiHooks {
-	public static function makeGlobalVariablesScript( &$vars ) {
-		global $wgExtensionAssetsPath;
-		$extensionPath = (!isset($wgExtensionAssetsPath) || $wgExtensionAssetsPath === false) ? $wgScriptPath . '/extensions' : $wgExtensionAssetsPath;
-		$wgDota2WebApiImagePath = $extensionPath . '/Dota2WebApi/modules/images/';
-
-		$vars['wgDota2WebApiImagePath'] = $wgDota2WebApiImagePath;
-		return true;
-	}
-	
-	public static function Dota2WebApiAddButtons() {
-		global $wgOut;
-		$wgOut->addModules('ext.dota2WebApi.toolbar');
-
-		return true;
-	}
-}
+/**
+ * Aliases
+ *
+ * @file
+ * @ingroup Extensions
+ */
+ 
+$specialPageAliases = array();
+ 
+/** English
+ * @author FO-nTTaX
+ */
+$specialPageAliases[ 'en' ] = array(
+	'ShowMatch' => array( 'ShowMatch', 'Showmatch' ),
+);
